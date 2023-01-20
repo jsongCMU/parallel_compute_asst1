e3f6036227e6376aa3e78647dadc53f424d2b997
Jaekyung Song <jaekyuns@andrew.cmu.edu>
2023-01-20 15:42:53 -0500

    Hardcode basic multi-threading

--- prog1_mandelbrot_threads/mandelbrot.cpp
+++ prog1_mandelbrot_threads/mandelbrot.cpp
@@ -121,8 +121,21 @@ void* workerThreadStart(void* threadArgs) {
     WorkerArgs* args = static_cast<WorkerArgs*>(threadArgs);
 
     // TODO: Implement worker thread here.
-
     printf("Hello world from thread %d\n", args->threadId);
+    float dx = (args->x1 - args->x0) / args->width;
+    float dy = (args->y1 - args->y0) / args->height;
+
+    int startRow = (args->threadId==0) ? (0)   : (450);
+    int endRow =   (args->threadId==0) ? (450) : (900);
+    for (int j = startRow; j < endRow; j++) {
+        for (int i = 0; i < args->width; ++i) {
+            float x = args->x0 + i * dx;
+            float y = args->y0 + j * dy;
+
+            int index = (j * args->width + i);
+            args->output[index] = mandel(x, y, args->maxIterations);
+        }
+    }
 
     return NULL;
 }
@@ -151,7 +164,13 @@ void mandelbrotThread(
 
     for (int i=0; i<numThreads; i++) {
         // TODO: Set thread arguments here.
+        args[i].x0 = x0; args[i].y0 = y0;
+        args[i].x1 = x1; args[i].y1 = y1;
+        args[i].width = width; args[i].height = height;
+        args[i].maxIterations = maxIterations;
+        args[i].output = output;
         args[i].threadId = i;
+        args[i].numThreads = numThreads;
     }
 
     // Fire up the worker threads.  Note that numThreads-1 pthreads
